dc1bb4f0e952865876ffce8be69c5e7aed7abb61
Saibal Thakur <saibalthakur@users.noreply.github.com>
2021-10-23 10:56:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function TurnOff() {
+    document.getElementById('bulb').src = 'https://s3-projectsassets.whjr.online/Off+Bulb.png'
+}
+
+function TurnOn() {
+    document.getElementById('bulb').src = 'https://s3-projectsassets.whjr.online/On+Bulb.png'
+}
